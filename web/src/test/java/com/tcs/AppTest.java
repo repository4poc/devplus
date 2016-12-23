6a94a8f13bc59b05103e546479eb05dc29c89a01
varinder gupta <varinder.gupta@tcs.com>
2016-12-23 16:15:52 +0530

    added devplus demo code

---
+++ web/src/test/java/com/tcs/AppTest.java
@@ -0,0 +1,22 @@
+package com.tcs;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void testApp() {
+		App appObject = new App();
+		Assert.assertEquals(appObject.reverseString("Test!"), "!tseT");
+	}
+
+	@Test
+	public void test2App() {
+		App appObject = new App();
+		Assert.assertEquals(appObject.reverseString("12321"), "12321");
+	}
+}
